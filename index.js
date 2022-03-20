1633ee62c85cf5575fa57428919809dc776e21c5
sabrina <sabrinadjebbar93@gmail.com>
2022-03-20 14:23:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+ let cards = ["Guadalupe", "Ollie", "Aki"]
+ let event = "surprise";
+ let newArray = [];
+
+  function writeCards(cards, event){
+    for (let i= 0; i < cards.length; i++) {
+        console.log( newArray.push (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`));
+    }    
+       return newArray;
+ }
+  
+ function countDown(){
+    let i = 10
+   while(i<=10, i>=0){
+      console.log(i--);
+   }
+
+ }
+
+    
+    
+     
+ 
+
+ 
+  
+
+   
+
+
+    
+
+
+
+
